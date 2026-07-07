0c7f8ceb972a3bd46586d73248e1b5e1d477e1ac
LOKESH <prajapatilokesh158@gmail.com>
2026-07-07 22:03:26 +0530

    Implement longestOnes function for maximum consecutive ones

---
+++ replace concept/maximum consecutive ones III(1004).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l=0;
+        long long sum=0;
+        int ans=0;
+        for(int r=0;r<nums.size();r++){
+            sum+=nums[r];
+            while(r-l+1-sum>k){
+                sum-=nums[l];
+                l++;
+            }
+            ans=max(ans,r-l+1);
+        }
+        return ans;
+    }
+
+};
